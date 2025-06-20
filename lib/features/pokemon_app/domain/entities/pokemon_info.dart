8f708d6ce651ffd85f15fe8a5efb513ca6e63565
Victor78970513 <yer29.chok@gmail.com>
2025-06-19 23:38:00 -0400

    pokemon info model added

--- lib/features/pokemon_app/domain/entities/pokemon_info.dart
+++ lib/features/pokemon_app/domain/entities/pokemon_info.dart
@@ -1,4 +1,34 @@
+import 'package:riverpod_practice/features/pokemon_app/domain/entities/pokemon_type.dart';
+
 class PokemonInfo {
+  //ABOUT
   final int id;
   final String name;
+  final double height;
+  final double weight;
+  final String description;
+  final List<PokemonType> againstTo;
+
+  // Status
+  final int hp;
+  final int atk;
+  final int def;
+  final int satk;
+  final int sdef;
+  final int spd;
+
+  PokemonInfo({
+    required this.id,
+    required this.name,
+    required this.height,
+    required this.weight,
+    required this.description,
+    required this.againstTo,
+    required this.hp,
+    required this.atk,
+    required this.def,
+    required this.satk,
+    required this.sdef,
+    required this.spd,
+  });
 }
